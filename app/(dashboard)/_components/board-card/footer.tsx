dbe92cc3b09c1a9e916c3d633396c4cfc44604af
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-24 20:08:07 +0500

    added

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -1,28 +1,41 @@
-
-
-
+import { cn } from '@/lib/utils'
 import React from 'react'
+import {Star} from 'lucide-react';
 
 type Props = {
-    title: string;
-    authorLabel: string;
-    createdAtLabel: string;
-    isFavorite: boolean;
-    onClick: ()=> void;
-    disabled:boolean
+  title: string,
+  authorLabel: string,
+  createdAtLabel: string,
+  isFavorite: boolean,
+  onClick: () => void,
+  disabled: boolean
 }
 
-export default function Footer({
-authorLabel,createdAtLabel,disabled,isFavorite,onClick,title
+export default function Footer ({
+  authorLabel,
+  createdAtLabel,
+  disabled,
+  isFavorite,
+  onClick,
+  title
 }: Props) {
   return (
-    <div className="relative bg-white p-3">
-<p className="text-[13px] truncate max-w-[100%-20px]">
-    {title}
-</p>
-<p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncated">
-    {authorLabel}, {createdAtLabel}
-</p>
+    <div className='relative bg-white p-3'>
+      <p className='text-[13px] truncate max-w-[100%-20px]'>{title}</p>
+      <p className='opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncated'>
+        {authorLabel}, {createdAtLabel}
+      </p>
+
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        className={cn (
+          'opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600',
+          disabled && 'cursor-not-allowed opacity-75'
+        )}
+      >
+        <Star className={cn("h-4 w-4",isFavorite && "fill-blue-600 text-blue-600")}/>
+      </button>
     </div>
   )
 }
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -7,6 +7,7 @@ import EmptyBoards from './empty-boards'
 import { useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import BoardCard from './board-card'
+import NewBoardButton from './NewBoardButton';
 
 type Props = {
   orgId: string,
@@ -42,6 +43,7 @@ export default function BoardList ({ orgId, query }: Props) {
       </h2>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
+        <NewBoardButton orgId={orgId}/>
         {data?.map(board => (
           <BoardCard
             key={board._id}
