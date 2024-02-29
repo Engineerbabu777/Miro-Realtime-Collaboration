9a8697331a713b811bc2405d215eadb5e00df15d
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-29 19:47:45 +0500

    added

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -21,3 +21,11 @@ export default function Toolbar ({}: Props) {
     </div>
   )
 }
+
+
+
+export const ToolbarSkeleton = () => {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
+  );
+};
--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,20 +1,21 @@
+import Canvas from './_components/canvas'
 
-
-import Canvas from './_components/canvas';
-
+import { Room } from '@/components/room'
+import Loading from './_components/canvas-loading'
 
 type Props = {
-    params:{
-        boardId: string;
-    }
+  params: {
+    boardId: string
+  }
 }
 
-export default function Page({
-    params 
-}:Props) {
-
-
-    return(<>
-    <Canvas boardId={params.boardId}/>
-    </>)
+export default function Page ({ params }: Props) {
+  return (
+    <>
+      <Room roomId={params.boardId} 
+      fallback={<Loading />}>
+        <Canvas boardId={params.boardId} />
+      </Room>
+    </>
+  )
 }
