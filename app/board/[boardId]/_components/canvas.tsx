effcfe37de51f0b97ad7ff2736d5f818d3748b3c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-01 23:59:56 +0500

    Added

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useSelf } from '@/liveblocks.config'
 import Info from './info'
 import Participants from './participants'
 import Toolbar from './toolbar'
@@ -8,6 +9,9 @@ type Props = {
   boardId: string
 }
 export default function Canvas ({ boardId }: Props) {
+
+  const info = useSelf(me => me.info)
+
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none'>
       <Info />
