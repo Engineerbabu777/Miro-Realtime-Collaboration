d5a7ab9bb6635083a44228f605acee4ae33ea0c3
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-04 23:53:15 +0500

    added

--- app/boards/[boardId]/_components/participants.tsx
+++ app/boards/[boardId]/_components/participants.tsx
@@ -2,6 +2,7 @@
 
 import { useOthers, useSelf } from '@/liveblocks.config'
 import { UserAvatar } from './user-avatar'
+import { connectionIdToColor } from '@/lib/utils'
 
 const MAX_SHOWN_USERS = 2
 
@@ -17,6 +18,7 @@ export default function Participants () {
         {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
           return (
             <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
               key={connectionId}
               src={info?.picture}
               name={info?.name}
@@ -28,6 +30,8 @@ export default function Participants () {
         {/* FOR DISPLAYING ME! */}
         {currentUser && (
           <UserAvatar
+          borderColor={connectionIdToColor(currentUser.connectionId)}
+
             src={currentUser?.info?.picture}
             name={currentUser?.info?.name + ' You'}
             fallback={currentUser?.info?.name?.[0]}
