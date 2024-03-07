0c3cea66a47eff878846b645a263d75bd3f94e60
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-07 17:50:43 +0500

    added

---
+++ app/boards/[boardId]/_components/CursorsPresence.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useOthersConnectionIds } from "@/liveblocks.config"
+import { memo, useMemo } from "react";
+import { Cursor } from "./Cursor";
+
+
+
+
+
+const Cursors = () => {
+    const ids = useOthersConnectionIds();
+
+    return(<>
+    {ids.map(connectionId => (<>
+    <Cursor 
+    key={connectionId}
+    connectionId={connectionId}
+    />
+    </>))
+
+    }
+    </>)
+}
+
+export const CursorPresence = memo(() => {
+    return(<>
+    <Cursors />
+    </>)
+})
+
+
+CursorPresence.displayName = 'CursorPresence';
